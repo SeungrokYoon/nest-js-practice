049fe9cb075d663b2ebccf3df8fc5b856cd14fb8
SeungrokYoon <sonicrok93@gmail.com>
2023-10-13 17:06:28 +0900

    feat: add update test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -79,4 +79,25 @@ describe('MoviesService', () => {
       expect(afterCreate).toBeGreaterThan(beforeCreate);
     });
   });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        title: 'Test Movies',
+        genres: ['test'],
+        year: 2000,
+      });
+      service.update(1, { title: 'Updated Test' });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Updated Test');
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
 });
